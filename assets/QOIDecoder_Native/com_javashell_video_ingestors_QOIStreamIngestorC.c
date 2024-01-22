edc1b6751cc105850f35118490e184f001e97c8e
dan <dan@localhost>
2024-01-21 19:33:29 -0500

    2

--- assets/QOIDecoder_Native/com_javashell_video_ingestors_QOIStreamIngestorC.c
+++ assets/QOIDecoder_Native/com_javashell_video_ingestors_QOIStreamIngestorC.c
@@ -2,15 +2,15 @@
 #include "qoi.h"
 #include "com_javashell_video_ingestors_QOIStreamIngestorC.h"
 #include <jni.h>
+#include <stdint.h>
 #include <cstdlib>
 
 JNIEXPORT jbyteArray JNICALL Java_com_javashell_video_ingestors_QOIStreamIngestorC_decode
   (JNIEnv* env, jobject obj, jbyteArray encodedData, jint encodedDataSize){
-    void* data = (void*) env->GetByteArrayElements(encodedData, NULL);
+    uint8_t* data = (uint8_t*) env->GetByteArrayElements(encodedData, NULL);
     qoi_desc* desc;
 
-    void* bytes;
-    bytes = qoi_decode(data, encodedDataSize, desc, 4);
+    uint8_t* bytes = (uint8_t*) qoi_decode(data, encodedDataSize, desc, 4);
 
     size_t raw_image_length = (size_t)desc->width * (size_t)desc->height * (size_t)desc->channels;
     size_t bytes_length = (size_t) (raw_image_length * sizeof(unsigned char));
